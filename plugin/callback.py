21075da5b0f9e03a85df876cf537d3be6235c425
HaomingJu <juhaoming@trunk.tech>
2022-06-23 23:21:42 +0800

    保存merge request详细信息

--- plugin/callback.py
+++ plugin/callback.py
@@ -1,3 +1,7 @@
+import os
+
+db_path = os.path.dirname(os.path.abspath(__file__)) + '/.db'
+
 def version(response):
     if response.status_code == 200:
         return True
@@ -9,6 +13,11 @@ def mr(response):
         mr_sources = []
         for obj in response.json():
             mr_sources.append("{} {}".format(obj["id"], obj["title"]))
+            mr_file = "{}/{}".format(db_path, obj["id"])
+            with open(mr_file, 'w') as mr_handle:
+                mr_handle.write("** {} ** \n\n".format(obj["title"]))
+                mr_handle.write("@{} ({} >>> {})\n\n".format(obj["author"]["name"], obj["source_branch"], obj["target_branch"]))
+                mr_handle.write(obj["description"])
 
         return mr_sources
     else:
