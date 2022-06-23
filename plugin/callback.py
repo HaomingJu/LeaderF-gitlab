bc3093e318a6dcc2bd55fc706a39922eb520b6f8
HaomingJu <juhaoming@trunk.tech>
2022-06-23 16:09:50 +0800

    显示条目增加前缀, MR的全局ID

--- plugin/callback.py
+++ plugin/callback.py
@@ -7,7 +7,7 @@ def version(obj):
 def mr(objs):
     mr_sources = []
     for obj in objs:
-        mr_sources.append(obj["title"])
+        mr_sources.append("{} {}".format(obj["id"], obj["title"]))
         logging.info(obj["title"])
 
     return mr_sources
